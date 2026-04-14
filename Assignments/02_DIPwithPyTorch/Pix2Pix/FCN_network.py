78dacc8ddcefe90ac96e5313ce47c52a6cd97787
WenhaoWei <weiw28941@gmail.com>
2026-04-14 16:56:15 +0800

    Hw2_new model

--- Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
+++ Assignments/02_DIPwithPyTorch/Pix2Pix/FCN_network.py
@@ -7,44 +7,54 @@ class FullyConvNetwork(nn.Module):
         super().__init__()
         # Encoder (Convolutional Layers)
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 8
-            nn.BatchNorm2d(8),
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1),  # Input channels: 3, Output channels: 64
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
         self.conv2 = nn.Sequential(
-            nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=1),  # 256 -> 128 -> 64
-            nn.BatchNorm2d(16),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # 256 -> 128 -> 64
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True)
         )
         self.conv3 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),  # 64 -> 32
-            nn.BatchNorm2d(32),
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),  # 64 -> 32
+            nn.BatchNorm2d(256),
             nn.ReLU(inplace=True)
         )
         self.conv4 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),  # 32 -> 16
-            nn.BatchNorm2d(64),
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),  # 32 -> 16
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1),  # 16 -> 8
+            nn.BatchNorm2d(512),
             nn.ReLU(inplace=True)
         )
         
         # Decoder (Deconvolutional Layers)
+        self.deconv5 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),  # 8 -> 16
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True)
+        )
         self.deconv4 = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # 16 -> 32
-            nn.BatchNorm2d(32),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),  # 16 -> 32
+            nn.BatchNorm2d(128),
             nn.ReLU(inplace=True)
         )
         self.deconv3 = nn.Sequential(
-            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),  # 32 -> 64
-            nn.BatchNorm2d(16),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # 32 -> 64
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
         self.deconv2 = nn.Sequential(
-            nn.ConvTranspose2d(16, 8, kernel_size=4, stride=2, padding=1),  # 64 -> 128
-            nn.BatchNorm2d(8),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # 64 -> 128
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True)
         )
         self.deconv1 = nn.Sequential(
-            nn.ConvTranspose2d(8, 3, kernel_size=4, stride=2, padding=1),  # 128 -> 256, output RGB channels
+            nn.ConvTranspose2d(32, 3, kernel_size=4, stride=2, padding=1),  # 128 -> 256, output RGB channels
             nn.Tanh()  # Output should be in [-1, 1] range to match normalized input
         )
 
@@ -54,9 +64,10 @@ class FullyConvNetwork(nn.Module):
         enc2 = self.conv2(enc1)
         enc3 = self.conv3(enc2)
         enc4 = self.conv4(enc3)
-        
+        enc5 = self.conv5(enc4)
         # Decoder forward pass
-        dec4 = self.deconv4(enc4)
+        dec5 = self.deconv5(enc5)
+        dec4 = self.deconv4(dec5)
         dec3 = self.deconv3(dec4)
         dec2 = self.deconv2(dec3)
         output = self.deconv1(dec2)
